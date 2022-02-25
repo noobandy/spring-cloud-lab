628cea5842d979376bf165cf1cbaf3194697777f
Anand Mohan <noobandy1364@gmail.com>
2022-02-25 12:43:44 +0530

    External config using AWS parameter store

--- ping/src/main/java/click/techlabs/ping/service/PingService.java
+++ ping/src/main/java/click/techlabs/ping/service/PingService.java
@@ -1,11 +1,16 @@
 package click.techlabs.ping.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
+@RefreshScope
 @Service
 public class PingService {
 
+	@Value("${message:Ping}")
+	private String message;
 	public String ping() {
-		return "Ping";
+		return message;
 	}
 }
--- pong/src/main/java/click/techlabs/pong/service/PongService.java
+++ pong/src/main/java/click/techlabs/pong/service/PongService.java
@@ -1,12 +1,15 @@
 package click.techlabs.pong.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PongService {
 
+	@Value("${message:Pong}")
+	private String message;
 	
 	public String pong() {
-		return "Pong";
+		return message;
 	}
 }
